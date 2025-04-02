ed7428ad1947d16f8a1fc62ac8b7924cfafdc4e2
Al_Hadi <mdabdullahalhadi408609@gmail.com>
2025-04-02 22:38:24 +0600

    Initial Code

---
+++ 2095B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define be(x) x.begin(), x.end()
+#define rbe(x) x.rbegin(), x.rend()
+#define pb push_back
+#define pf push_front
+#define ppf pop_front
+#define ppb pop_back
+#define ft front
+#define bk back
+using namespace std;
+int main()
+{
+    string s;
+    int x;
+    cin>>s>>x;
+    cout<<10000<<endl;
+}
