986243b33e736d32340689497aa71fc145cbed1c
Al_Hadi <mdabdullahalhadi408609@gmail.com>
2025-04-02 21:43:39 +0600

    Initial Code

---
+++ 2095A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define be(x) x.begin(), x.end()
+#define rbe(x) x.rbegin(), x.rend()
+#define pb push_back
+#define pf push_front
+#define ppf pop_front
+#define ppb pop_back
+#define ft front
+#define bk back
+using namespace std;
+int main()
+{
+    cout<<"puzzling"<<endl;
+
+}
