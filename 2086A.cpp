bc76a6de3933482fca5f241b686f81e3eaead344
Al_Hadi <mdabdullahalhadi408609@gmail.com>
2025-04-06 12:42:17 +0600

    Initial Code

---
+++ 2086A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long 
+#define be(x) x.begin() , x.end()
+#define rbe(x) x.begin() , x.end()
+#define pb push_back
+using namespace std;
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+      int n;
+      cin>>n;
+      cout<<n*2<<endl;
+   }
+
+} 
