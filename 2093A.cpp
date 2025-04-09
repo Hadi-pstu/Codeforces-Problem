3e96e3e5ab9f0a70332754b875620bda7fab1b81
Al_Hadi <mdabdullahalhadi408609@gmail.com>
2025-04-09 10:31:05 +0600

    Initial Code

---
+++ 2093A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define be(x) x.begin(), x.end()
+#define rbe(x) x.rbegin(), x.rend()
+#define pb push_back
+#define pf push_front
+#define ppf pop_front
+#define ppb pop_back
+#define ft front
+#define bk back
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n%2!=0)
+        {
+ 
+            cout<<"Yes"<<endl;
+        }
+        else
+        {
+ 
+            cout<<"No"<<endl;
+        }
+    }
+}
+
