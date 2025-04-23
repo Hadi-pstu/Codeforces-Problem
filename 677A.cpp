ac554ef82b7c4c51dcc50affbfea68ec3a5ac868
Al_Hadi <mdabdullahalhadi408609@gmail.com>
2025-04-23 15:20:35 +0600

    Initial Code

---
+++ 677A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define be(x) x.begin(), x.end()
+#define rbe(x) x.rbegin(), x.rend()
+#define pb push_back
+#define pf push_front
+#define ppf pop_front
+#define ppb pop_back
+#define ft front
+#define bk back
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    
+    int i,n,h,coun=0;
+    cin>>n>>h;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        if(h>=arr[i])
+            coun++;
+        else
+            coun +=2;
+    }
+    cout<<coun;
+}
