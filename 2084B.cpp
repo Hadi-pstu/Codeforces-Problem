32297e001a7f3a64293cbf775947b47fe38429cc
Al_Hadi <mdabdullahalhadi408609@gmail.com>
2025-05-07 20:47:09 +0600

    Initial code

---
+++ 2084B.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define be(x) x.begin(), x.end()
+#define rbe(x) x.rbegin(), x.rend()
+#define pb push_back
+#define pf push_front
+#define ppf pop_front
+#define ppb pop_back
+#define ft front
+#define bk back
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        vector<ll>v(n);
+        
+        for(ll i=0;i<n;i++)
+        {
+            cin>>v[i];
+        }
+        sort(be(v));
+        ll p = min_element(be(v) )- v.begin();
+        ll g = 0;
+        for(ll i=0;i<n;i++)
+        {
+            if( i != 0 && v[i] % v[p] == 0)
+            {
+                g = __gcd(v[i],g);
+            }
+              
+        }
+        if(g == v[p])
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+
+       
+    }
+    
+}
