83257b05302090ded4c38d2fd95f7893691be312
Al_Hadi <mdabdullahalhadi408609@gmail.com>
2025-05-09 21:07:14 +0600

    Initial Code

---
+++ 1991A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define be(x) x.begin(), x.end()
+#define rbe(x) x.rbegin(), x.rend()
+#define pb push_back
+#define pf push_front
+#define ppf pop_front
+#define ppb pop_back
+#define ft front
+#define bk back
+using namespace std;
+
+void yes()
+{
+  cout<<"YES"<<endl;
+}
+void no()
+{
+  cout<<"NO"<<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>v[i];
+        }
+        vector<int>mx(n);
+        for(int i=0;i<n;i+=2)
+        {
+           // if(i % 2 == 0)
+            mx[i] = v[i];
+        }
+        sort(rbe(mx));
+        cout<<mx[0]<<endl;
+    }
+
+        
+}
+
