4483584c9ac4b687e20a4c56cc8d7fa337f1e741
MD. Abdullah Al Hadi <mdabdullahalhadi408609@gmail.com>
2025-05-22 00:40:18 +0600

    Initial Code

--- 1952A.cpp
+++ 1952A.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define be(x) x.begin(), x.end()
+#define rbe(x) x.rbegin(), x.rend()
+#define pb push_back
+#define pf push_front
+#define ppf pop_front
+#define ppb pop_back
+#define ft front
+#define bk back
+using namespace std;
+void yes()
+{
+  cout<<"YES"<<endl;
+}
+void no()
+{
+  cout<<"NO"<<endl;
+}
+
+int main()
+{
+     cout<<"safety"<<endl;
+  
+}
 
